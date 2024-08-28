fc3f157409dba27ec7e5136cbf7310e12c18e6cb
Ian Claggett <claggett.32@buckeyemail.osu.edu>
2024-08-28 12:19:51 -0400

    updated research paper display and uploaded documents

--- js/carousel.js
+++ js/carousel.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
     $("body > div:nth-child(3) > div:nth-child(2) > div:nth-child(2)").hide();
     $("body > div:nth-child(3) > div:nth-child(3) > div:nth-child(2)").hide();
     $("body > div:nth-child(3) > div:nth-child(4) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(5) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(6) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(7) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(8) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(9) > div:nth-child(2)").hide();
+    $("body > div:nth-child(3) > div:nth-child(10) > div:nth-child(2)").hide();
 
     $("body > div:nth-child(7) > div:nth-child(1) > div:nth-child(2)").hide();
     $("body > div:nth-child(7) > div:nth-child(2) > div:nth-child(2)").hide();
@@ -14,11 +20,10 @@ $(document).ready(function () {
     $("body > div:nth-child(7) > div:nth-child(6) > div:nth-child(2)").hide();
     $("body > div:nth-child(7) > div:nth-child(7) > div:nth-child(2)").hide();
     $("body > div:nth-child(7) > div:nth-child(8) > div:nth-child(2)").hide();
+    $("body > div:nth-child(7) > div:nth-child(9) > div:nth-child(2)").hide();
 
     $("body > div:nth-child(13) > div:nth-child(1) > div:nth-child(2)").hide();
     $("body > div:nth-child(13) > div:nth-child(2) > div:nth-child(2)").hide();
-    $("body > div:nth-child(13) > div:nth-child(3) > div:nth-child(2)").hide();
-    $("body > div:nth-child(13) > div:nth-child(4) > div:nth-child(2)").hide();
 
     //Button triggers
     $("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)").click(function(){
@@ -41,7 +46,37 @@ $(document).ready(function () {
         $("body > div:nth-child(3) > div:nth-child(4) > div:nth-child(2)").slideToggle();
     });
 
+    $("body > div:nth-child(3) > div:nth-child(5) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(5) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(5) > div:nth-child(2)").slideToggle();
+    });
 
+    $("body > div:nth-child(3) > div:nth-child(6) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(6) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(6) > div:nth-child(2)").slideToggle();
+    });
+
+    $("body > div:nth-child(3) > div:nth-child(7) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(7) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(7) > div:nth-child(2)").slideToggle();
+    });
+
+    $("body > div:nth-child(3) > div:nth-child(8) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(8) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(8) > div:nth-child(2)").slideToggle();
+    });
+
+    $("body > div:nth-child(3) > div:nth-child(9) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(9) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(9) > div:nth-child(2)").slideToggle();
+    });
+
+    $("body > div:nth-child(3) > div:nth-child(10) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(3) > div:nth-child(10) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(3) > div:nth-child(10) > div:nth-child(2)").slideToggle();
+    });
+
+    //Working Papers
     $("body > div:nth-child(7) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)").click(function(){
         $("body > div:nth-child(7) > div:nth-child(1) > div:nth-child(2)").stop(true);
         $("body > div:nth-child(7) > div:nth-child(1) > div:nth-child(2)").slideToggle();
@@ -82,7 +117,12 @@ $(document).ready(function () {
         $("body > div:nth-child(7) > div:nth-child(8) > div:nth-child(2)").slideToggle();
     });
 
+    $("body > div:nth-child(7) > div:nth-child(9) > div:nth-child(1) > button:nth-child(1)").click(function(){
+        $("body > div:nth-child(7) > div:nth-child(9) > div:nth-child(2)").stop(true);
+        $("body > div:nth-child(7) > div:nth-child(9) > div:nth-child(2)").slideToggle();
+    });
 
+    //Work in Progress
     $("body > div:nth-child(13) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)").click(function(){
         $("body > div:nth-child(13) > div:nth-child(1) > div:nth-child(2)").stop(true);
         $("body > div:nth-child(13) > div:nth-child(1) > div:nth-child(2)").slideToggle();
@@ -91,15 +131,5 @@ $(document).ready(function () {
     $("body > div:nth-child(13) > div:nth-child(2) > div:nth-child(1) > button:nth-child(1)").click(function(){
         $("body > div:nth-child(13) > div:nth-child(2) > div:nth-child(2)").stop(true);
         $("body > div:nth-child(13) > div:nth-child(2) > div:nth-child(2)").slideToggle();
-    });
-
-    $("body > div:nth-child(13) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)").click(function(){
-        $("body > div:nth-child(13) > div:nth-child(3) > div:nth-child(2)").stop(true);
-        $("body > div:nth-child(13) > div:nth-child(3) > div:nth-child(2)").slideToggle();
-    });
-
-    $("body > div:nth-child(13) > div:nth-child(4) > div:nth-child(1) > button:nth-child(1)").click(function(){
-        $("body > div:nth-child(13) > div:nth-child(4) > div:nth-child(2)").stop(true);
-        $("body > div:nth-child(13) > div:nth-child(4) > div:nth-child(2)").slideToggle();
     });
 });
