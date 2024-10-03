866843255dfa5c608ea3bd12e0c2a6ae06541e52
Ian Claggett <claggett.32@buckeyemail.osu.edu>
2024-10-03 14:47:38 -0400

    updated home page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
 $(document).ready(function () {
-    $(".container-fluid > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)").hide();
-    $(".container-fluid > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)").hide();
-    $("div.row:nth-child(3) > div:nth-child(1) > div:nth-child(2)").hide();
-    $("div.row:nth-child(3) > div:nth-child(2) > div:nth-child(2)").hide();
+    $(".index_cont1 > div:nth-child(2)").hide();
+    $(".index_cont2 > div:nth-child(2)").hide();
+    $(".index_cont3 > div:nth-child(2)").hide();
+    $(".index_cont4 > div:nth-child(2)").hide();
 
-    $(".container-fluid > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)").on("mouseenter", function () {
-        $(".container-fluid > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)").fadeIn();
-    })
+    //Research
+    $(".index_cont1 > img:nth-child(1)").on("mouseenter", function () {
+        $(".index_cont1 > div:nth-child(2)").stop(); 
+        $(".index_cont1 > div:nth-child(2)").fadeIn();
+    });
 
+    $(".index_cont1").on("mouseleave",function(){   
+        $(".index_cont1 > div:nth-child(2)").fadeOut();
+    });
+
+    //Teaching
+    $(".index_cont2 > img:nth-child(1)").on("mouseenter", function () {
+        $(".index_cont2 > div:nth-child(2)").stop(); 
+        $(".index_cont2 > div:nth-child(2)").fadeIn();
+    });
+
+    $(".index_cont2").on("mouseleave",function(){   
+        $(".index_cont2 > div:nth-child(2)").fadeOut();
+    });
+
+    //Data
+    $(".index_cont3 > img:nth-child(1)").on("mouseenter", function () {
+        $(".index_cont3 > div:nth-child(2)").stop(); 
+        $(".index_cont3 > div:nth-child(2)").fadeIn();
+    });
+
+    $(".index_cont3").on("mouseleave",function(){   
+        $(".index_cont3 > div:nth-child(2)").fadeOut();
+    });
+
+    //CV
+    $(".index_cont4 > img:nth-child(1)").on("mouseenter", function () {
+        $(".index_cont4 > div:nth-child(2)").stop(); 
+        $(".index_cont4 > div:nth-child(2)").fadeIn();
+    });
+
+    $(".index_cont4").on("mouseleave",function(){   
+        $(".index_cont4 > div:nth-child(2)").fadeOut();
+    });
 });
